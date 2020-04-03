24e124066996fa7fb54adf8d04a1a9aa5f897850
httishere <18457730959@163.com>
2020-04-03 09:10:26 +0800

    theme set

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ViewUI from 'view-design';
+import locale from 'view-design/dist/locale/en-US';
 import "./theme/index.less";
+// import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false
+
+Vue.use(ViewUI, { locale });
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container container-home">
+    <div class="container-inner">
+      <!-- header -->
+      <Row class="part-nav">
+        <Col class="site-logo" span="12">
+          <div class="logo-inner">MewGulf</div>
+        </Col>
+        <Col class="site-nav" span="12">
+          <Menu mode="horizontal" :theme="theme1" active-name="1">
+            <MenuItem name="1">
+              <Icon type="md-people" />
+              双人介绍
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="md-alarm" />
+              时间线
+            </MenuItem>
+            <MenuItem name="4">
+              <Icon type="ios-planet-outline" />
+              QQ Love
+            </MenuItem>
+          </Menu>
+        </Col>
+      </Row>
+      <!-- head banner -->
+      <Row class="part-banner"></Row>
+      <!-- introduction and work -->
+      <Row class="introduction"></Row>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home"
+};
+</script>
+<style lang="less" scoped>
+.site-logo {
+  height: 60px;
+  padding-left: 60px;
+  &:after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 1px;
+    background: #dcdee2;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+  .logo-inner {
+    line-height: 60px;
+    font-size: 48px;
   }
 }
-</script>
+</style>
